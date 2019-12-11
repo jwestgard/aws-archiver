441f0c0a9affbaa8f5053438a0788e589d295da4
Peter Eichman <peichman@umd.edu>
2019-12-11 14:53:34 -0500

    Write line-oriented json logs.
    The JSON log file is named with the name of the batch.
    
    See also: http://jsonlines.org/

--- archiver/deposit.py
+++ archiver/deposit.py
@@ -73,6 +73,8 @@ def deposit(args):
     sys.stdout.write(f'  - Max Threads: {batch.max_threads}\n')
     sys.stdout.write(f'  - AWS Profile: {args.profile}\n\n')
 
+    json_logfile_name = os.path.join(batch.logdir, batch.name + '.json')
+
     if batch.mapfile:
         mapfile_path = os.path.join(batch.logdir, batch.mapfile + '.tmp')
         mapfile = open(mapfile_path, 'w+')
@@ -80,6 +82,9 @@ def deposit(args):
                         'keypath', 'etag', 'result']
         writer = csv.DictWriter(mapfile, fieldnames=fieldnames)
         writer.writeheader()
+    else:
+        mapfile = None
+        writer = None
 
     # Set up a session with specified authentication profile
     try:
@@ -92,82 +97,85 @@ def deposit(args):
     # Process and transfer each asset in the batch contents
     sys.stdout.write(f'Depositing {len(batch.contents)} assets ...\n')
 
-    for n, asset in enumerate(batch.contents, 1):
-        asset.header = f'({n}) {asset.filename.upper()}'
-        asset.key_path = f'{batch.name}/{asset.relpath}'
-        asset.expected_etag = calculate_etag(asset.local_path,
-                                             chunk_size=batch.chunk_bytes
-                                             )
+    with open(json_logfile_name, 'w') as json_log:
+        for n, asset in enumerate(batch.contents, 1):
+            asset.header = f'({n}) {asset.filename.upper()}'
+            asset.key_path = f'{batch.name}/{asset.relpath}'
+            asset.expected_etag = calculate_etag(asset.local_path,
+                                                 chunk_size=batch.chunk_bytes
+                                                 )
 
-        # Prepare custom metadata to attach to the asset
-        asset.extra_args = {'Metadata': {
-                                'md5': asset.md5,
-                                'bytes': str(asset.bytes)
-                                },
-                            'StorageClass': batch.storage_class
-        }
+            # Prepare custom metadata to attach to the asset
+            asset.extra_args = {'Metadata': {
+                                    'md5': asset.md5,
+                                    'bytes': str(asset.bytes)
+                                    },
+                                'StorageClass': batch.storage_class
+            }
 
-        # Display Asset information to the user
-        sys.stdout.write(f'\n{asset.header}\n{"=" * len(asset.header)}\n')
-        sys.stdout.write(f'    FILE: {asset.local_path}\n')
-        sys.stdout.write(f' KEYPATH: {asset.key_path}\n')
-        sys.stdout.write(f'     EXT: {asset.extension}\n')
-        sys.stdout.write(f'   MTIME: {asset.mtime}\n')
-        sys.stdout.write(f'   BYTES: {asset.bytes}\n')
-        sys.stdout.write(f'     MD5: {asset.md5}\n')
-        sys.stdout.write(f'    ETAG: {asset.expected_etag}\n\n')
+            # Display Asset information to the user
+            sys.stdout.write(f'\n{asset.header}\n{"=" * len(asset.header)}\n')
+            sys.stdout.write(f'    FILE: {asset.local_path}\n')
+            sys.stdout.write(f' KEYPATH: {asset.key_path}\n')
+            sys.stdout.write(f'     EXT: {asset.extension}\n')
+            sys.stdout.write(f'   MTIME: {asset.mtime}\n')
+            sys.stdout.write(f'   BYTES: {asset.bytes}\n')
+            sys.stdout.write(f'     MD5: {asset.md5}\n')
+            sys.stdout.write(f'    ETAG: {asset.expected_etag}\n\n')
 
-        # Send the file, optionally in multipart, multithreaded mode
-        progress_tracker = ProgressPercentage(asset)
-        try:
-            s3.meta.client.upload_file(
-                asset.local_path,
-                batch.bucket,
-                asset.key_path,
-                ExtraArgs=asset.extra_args,
-                Config=batch.aws_config,
-                Callback=progress_tracker
-            )
-        except S3UploadFailedError as e:
-            print(e, file=sys.stderr)
-            print('Continuing with the next asset', file=sys.stderr)
-            continue
+            # Send the file, optionally in multipart, multithreaded mode
+            progress_tracker = ProgressPercentage(asset)
+            try:
+                s3.meta.client.upload_file(
+                    asset.local_path,
+                    batch.bucket,
+                    asset.key_path,
+                    ExtraArgs=asset.extra_args,
+                    Config=batch.aws_config,
+                    Callback=progress_tracker
+                )
+            except S3UploadFailedError as e:
+                print(e, file=sys.stderr)
+                continue
 
-        # Validate the upload with a head request to get the remote Etag
-        sys.stdout.write('\n\n  Upload complete! Verifying...\n')
-        try:
-            response = s3.meta.client.head_object(Bucket=batch.bucket, Key=asset.key_path)
-        except ClientError as e:
-            print(f'Error verifying {batch.bucket}/{asset.key_path}: {e}', file=sys.stderr)
-            print('Continuing with the next asset', file=sys.stderr)
-            continue
+            # Validate the upload with a head request to get the remote Etag
+            sys.stdout.write('\n\n  Upload complete! Verifying...\n')
+            try:
+                response = s3.meta.client.head_object(Bucket=batch.bucket, Key=asset.key_path)
+            except ClientError as e:
+                print(f'Error verifying {batch.bucket}/{asset.key_path}: {e}', file=sys.stderr)
+                print('Continuing with the next asset', file=sys.stderr)
+                continue
 
-        # Pull the AWS etag from the response and strip quotes
-        headers = response['ResponseMetadata']['HTTPHeaders']
-        logfile = asset.key_path.replace('/', '-') + '.json'
-        logpath = os.path.join(batch.logdir, logfile)
-        with open(logpath, 'w+') as handle:
-            json.dump(response['ResponseMetadata'], handle, indent=2)
-        remote_etag = headers['etag'].replace('"', '')
-        sys.stdout.write(f'    -> Local:  {asset.expected_etag}\n')
-        sys.stdout.write(f'    -> Remote: {remote_etag}\n\n')
-        if remote_etag == asset.expected_etag:
-            sys.stdout.write(f'  ETag match! Transfer success!\n')
-            result = 'success'
-        else:
-            sys.stdout.write(f'  Something went wrong.\n')
-            result = 'failed'
+            # Write response metadata to a line-oriented JSON file
+            # See also: http://jsonlines.org/
+            json.dump({'asset': f'{batch.bucket}/{asset.key_path}', 'response': response['ResponseMetadata']}, json_log)
+            json_log.write('\n')
 
-        row = {
-            'id': n,
-            'relpath': asset.relpath,
-            'filename': asset.filename,
-            'md5': asset.md5,
-            'bytes': asset.bytes,
-            'keypath': asset.key_path,
-            'etag': remote_etag,
-            'result': result
-        }
-        writer.writerow(row)
+            # Pull the AWS etag from the response and strip quotes
+            headers = response['ResponseMetadata']['HTTPHeaders']
+            remote_etag = headers['etag'].replace('"', '')
+            sys.stdout.write(f'    -> Local:  {asset.expected_etag}\n')
+            sys.stdout.write(f'    -> Remote: {remote_etag}\n\n')
+            if remote_etag == asset.expected_etag:
+                sys.stdout.write(f'  ETag match! Transfer success!\n')
+                result = 'success'
+            else:
+                sys.stdout.write(f'  Something went wrong.\n')
+                result = 'failed'
 
-    mapfile.close()
+            row = {
+                'id': n,
+                'relpath': asset.relpath,
+                'filename': asset.filename,
+                'md5': asset.md5,
+                'bytes': asset.bytes,
+                'keypath': asset.key_path,
+                'etag': remote_etag,
+                'result': result
+            }
+            if writer is not None:
+                writer.writerow(row)
+
+    if mapfile is not None:
+        mapfile.close()
